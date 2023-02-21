36857afb35c9e0fd3692ca201c7c4e54abb8e4ac
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-21 13:11:34 +0100

    add some behavior

--- src/calculate/calculate.spec.ts
+++ src/calculate/calculate.spec.ts
@@ -1,7 +1,10 @@
 import { calculate } from './calculate';
 
 describe('Calculate a number', () => {
-  test('calculate "" return 0', async () => {
-    expect(calculate('')).toEqual(0);
+  test('calculate "0" return 0', async () => {
+    expect(calculate('0')).toEqual(0);
+  });
+  test('calculate "1" return 1', async () => {
+    expect(calculate('1')).toEqual(1);
   });
 });
