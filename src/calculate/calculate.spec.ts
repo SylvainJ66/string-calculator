7a3fccb953fcb755e9be95edf99d59c9a81a35a4
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-21 13:17:57 +0100

    add some behavior

--- src/calculate/calculate.spec.ts
+++ src/calculate/calculate.spec.ts
@@ -1,10 +1,18 @@
 import { calculate } from './calculate';
 
-describe('Calculate a number', () => {
+describe('Rule: Get Number from string', () => {
   test('calculate "0" return 0', async () => {
     expect(calculate('0')).toEqual(0);
   });
   test('calculate "1" return 1', async () => {
     expect(calculate('1')).toEqual(1);
+  });
+  test('calculate "" return 0', async () => {
+    expect(calculate('')).toEqual(0);
+  });
+});
+describe('Rule: Sum 2 numbers', () => {
+  test('calculate "1,2" return 3', async () => {
+    expect(calculate('1,2')).toEqual(3);
   });
 });
--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,3 +1,5 @@
 export function calculate(input: string) {
+  if(input[0] === ',')
+    return input[0] + input[2];
   return +input;
 }
