6810e2b8c9b758b168cf25313777f00fc09ef71f
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-22 22:07:22 +0100

    reboot and first commit

--- src/calculate/calculate.spec.ts
+++ src/calculate/calculate.spec.ts
@@ -5,22 +5,5 @@ describe('Feature: Calculate number', () => {
     test('calculate "0" return 0', async () => {
       expect(calculate('0')).toEqual(0);
     });
-    test('calculate "1" return 1', async () => {
-      expect(calculate('1')).toEqual(1);
-    });
-    test('calculate "" return 0', async () => {
-      expect(calculate('')).toEqual(0);
-    });
-    test('calculate "12" return 12', async () => {
-      expect(calculate('12')).toEqual(12);
-    });
-  });
-  describe('Rule: Sum 2 numbers', () => {
-    test('calculate "1,2" return 3', async () => {
-      expect(calculate('1,2')).toEqual(3);
-    });
-    test('calculate "12,2" return 14', async () => {
-      expect(calculate('12,2')).toEqual(14);
-    });
   });
 });
--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,6 +1,3 @@
 export function calculate(input: string) {
-  const indexComma = input.indexOf(',');
-  if(indexComma > -1)
-    return +input.substring(0, indexComma) + +input.substring(indexComma+1, input.length);
-  return +input;
+  return 0;
 }
