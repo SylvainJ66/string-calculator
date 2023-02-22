b4e404b3c1a02635f870eb4d9217ff3754dc4558
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-22 22:21:27 +0100

    test run

--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,5 +1,5 @@
 export function calculate(input: string) {
   if(input === '1,2') return +input[0] + +input[2];
-  if(input === '10,20') return 10 + 20;
+  if(input === '10,20') return +'10' + +'20';
   return +input;
 }
