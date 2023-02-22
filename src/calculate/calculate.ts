faaf09af723ee043203a4b59b374c3279c4f2927
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-22 22:52:43 +0100

    test run

--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,11 +1,10 @@
 export function calculate(input: string) {
   let comma = 1;
-  while (input[comma] === ',') {
-    return +input.slice(0, comma) + +input.slice(comma + 1, input.length);
-  }
-  comma = 2;
-  while (input[comma] === ',') {
-    return +input.slice(0, comma) + +input.slice(comma + 1, input.length);
+  while (input.length > comma) {
+    if (input[comma] === ',') {
+      return +input.slice(0, comma) + +input.slice(comma + 1, input.length);
+    }
+    comma = 2;
   }
 
   return +input;
