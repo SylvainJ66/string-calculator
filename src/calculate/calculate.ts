d8febe3999c0d6025d3f67d76c01ec830a55721d
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-22 22:42:22 +0100

    test run

--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,6 +1,6 @@
 export function calculate(input: string) {
   let comma = 1;
-  if (input[1] === ',') {
+  while (input[comma] === ',') {
     return +input.slice(0, comma) + +input.slice(comma + 1, input.length);
   }
   comma = 2;
