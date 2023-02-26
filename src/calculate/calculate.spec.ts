a2b1d1c63ae360a1fbcb6e1e291280f63380caca
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-26 16:59:48 +0100

    reload --------

--- src/calculate/calculate.spec.ts
+++ src/calculate/calculate.spec.ts
@@ -5,16 +5,5 @@ describe('Feature: Calculate number', () => {
     test('calculate "0" return 0', async () => {
       expect(calculate('0')).toEqual(0);
     });
-    test('calculate "1" return 1', async () => {
-      expect(calculate('1')).toEqual(1);
-    });
-  });
-  describe('Rule: Sum 2 numbers', () => {
-    test('calculate "1,2" return 3', async () => {
-      expect(calculate('1,2')).toEqual(3);
-    });
-    test('calculate "10,20" return 30', async () => {
-      expect(calculate('10,20')).toEqual(30);
-    });
   });
 });
