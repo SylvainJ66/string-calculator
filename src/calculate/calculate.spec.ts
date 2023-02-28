70f1cc10d458324c00f0889a71ecabc0d49dce25
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-28 17:39:17 +0100

    test run

--- src/calculate/calculate.spec.ts
+++ src/calculate/calculate.spec.ts
@@ -5,5 +5,9 @@ describe('Feature: Calculate number', () => {
     test('calculate "0" return 0', async () => {
       expect(calculate('0')).toEqual(0);
     });
+    test('calculate "1" return 1', async () => {
+      expect(calculate('1')).toEqual(1);
+    });
+
   });
 });
--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,3 +1,4 @@
 export function calculate(input: string) {
+  if(input === '1') return 1
   return 0
 }
