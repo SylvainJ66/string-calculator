f1ffd34a61632015352f6c0f9032d708f1aa6f1e
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-28 18:22:17 +0100

    test run

--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,11 +1,4 @@
 export function calculate(input: string) {
   let i = input.indexOf(',');
-  if(input[i] === ',') {
-    return +input.slice(0, i) + +input.slice(i + 1, input.length);
-  }
-  i = 2;
-  if(input[i] === ',') {
-    return +input.slice(0, i) + +input.slice(i + 1, input.length);
-  }
-  return +input
+  return +input.slice(0, i) + +input.slice(i + 1, input.length);
 }
