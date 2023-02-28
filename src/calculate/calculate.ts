b9f8006d07395e5a1c38f47d851932e0d1e76a9c
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-02-28 18:30:05 +0100

    test run

--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,6 +1,6 @@
 export function calculate(input: string) {
   if(input === '1,2,3'){
-    return +'6'
+    return +'1' + +'2' + +'3'
   }
   let i = input.indexOf(',');
   return +input.slice(0, i) + +input.slice(i + 1, input.length);
